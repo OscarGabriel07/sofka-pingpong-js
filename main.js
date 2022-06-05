57f7898c111ff917ce43a57b228a17ca0b9e6e0d
Oscar Farfan <oscarfarfan92@gmail.com>
2022-06-05 11:45:36 -0500

    Se crea el movimiento de las barras internas del tablero y se crea la función pelota.

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 /**
- * Función que define la altura y la anchura del tablero del juego
+ * Función Board define la altura y la anchura del tablero del juego
+ * 
+ * Función Ball define los atributos de la pelota
  */
 (function () {
     self.Board = function (width, height) {
@@ -19,6 +21,20 @@
         }
     }
 })();
+(function () {
+    self.Ball = function (x, y, radius, board) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed_y = 0;
+        this.speed_x = 3;
+        this.board = board;
+
+        board.ball = this;
+        this.kind = "circle";
+    }
+})();
+
 
 /**
  * Función que define las barras del juego y que son insertadas en el tablero del juego
@@ -32,7 +48,7 @@
         this.board = board;
         this.board.bars.push(this);
         this.kind = "rectangle";
-        this.speed = 10;
+        this.speed = 5;
     }
 
     /**
@@ -64,48 +80,63 @@
     }
 
     self.BoardView.prototype = {
+        clean: function () {
+            this.ctx.clearRect(0, 0, board.width, board.height);
+        },
         draw: function () {
             for (var i = this.board.elements.length - 1; i >= 0; i--) {
                 var el = this.board.elements[i];
                 
                 draw(this.ctx, el);
             }
+        },
+        play: function () {
+            this.clean();
+            this.draw();
         }
     }
 
     function draw(ctx, element) {
-        if (element !== null && element.hasOwnProperty("kind")) {
-            switch (element.kind) {
-                case "rectangle":
+        switch (element.kind) {
+            case "rectangle":
                 ctx.fillRect(element.x, element.y, element.width, element.height);
                 break;
-            }
+            case "circle":
+                ctx.beginPath();
+                ctx.arc(element.x, element.y, element.radius, 0, 7);
+                ctx.fill();
+                ctx.closePath();
+                break;
         }
     }
 })();
 
 var board = new Board(800, 400);
 var bar = new Bar(20, 100, 40, 100, board);
-var bar = new Bar(735, 100, 40, 100, board);
+var bar_2 = new Bar(735, 100, 40, 100, board);
 var canvas = document.getElementById('canvas');
 var board_view = new BoardView(canvas, board);
+var ball = new Ball(350, 100, 10, board);
 
 document.addEventListener("keydown", function (ev) {
+    ev.preventDefault();
     if (ev.keyCode == 38) {
         bar.up();
     } else if (ev.keyCode == 40) {
         bar.down();    
+    } else if (ev.keyCode === 87) {
+        bar_2.up();
+    } else if (ev.keyCode === 83) {
+        bar_2.down();
     }
-
-    console.log("" + bar);
 });
 
-self.addEventListener("load", main);
+window.requestAnimationFrame(Controller);
 
 /**
  * Función principal del programa, desde donde se crean los elementos del juego 
  */
-function main() {
-    console.log(board);
-    board_view.draw();
+function Controller() {
+    board_view.play();
+    window.requestAnimationFrame(Controller);
 }
