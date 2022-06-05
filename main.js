ce567efca61e3aad8921a02befc1122440f5b83f
Oscar Farfan <oscarfarfan92@gmail.com>
2022-06-05 12:08:11 -0500

    Se crea funcionalidad para que la pelota se mueva.

--- main.js
+++ main.js
@@ -11,11 +11,12 @@
         this.game_over = false;
         this.bars = [];
         this.ball = null;
+        this.playing = false;
     }
 
     self.Board.prototype = {
         get elements() {
-            var elements = this.bars;
+            var elements = this.bars.map(function(bar){return bar;});
             elements.push(this.ball);
             return elements;
         }
@@ -29,9 +30,17 @@
         this.speed_y = 0;
         this.speed_x = 3;
         this.board = board;
+        this.direction = 1;
 
         board.ball = this;
         this.kind = "circle";
+
+    }
+    self.Ball.prototype = {
+        move: function () {
+            this.x += (this.speed_x * this.direction);
+            this.y += (this.speed_y * this.direction);
+        }
     }
 })();
 
@@ -91,8 +100,11 @@
             }
         },
         play: function () {
-            this.clean();
-            this.draw();
+            if (this.board.playing) {
+                this.clean();
+                this.draw();
+                this.board.ball.move();
+            }
         }
     }
 
@@ -119,20 +131,27 @@ var board_view = new BoardView(canvas, board);
 var ball = new Ball(350, 100, 10, board);
 
 document.addEventListener("keydown", function (ev) {
-    ev.preventDefault();
     if (ev.keyCode == 38) {
+        ev.preventDefault();
         bar.up();
     } else if (ev.keyCode == 40) {
+        ev.preventDefault();
         bar.down();    
     } else if (ev.keyCode === 87) {
+        ev.preventDefault();
         bar_2.up();
     } else if (ev.keyCode === 83) {
+        ev.preventDefault();
         bar_2.down();
+    } else if (ev.keyCode === 32) {
+        ev.preventDefault();
+        board.playing = !board.playing;
     }
 });
 
 window.requestAnimationFrame(Controller);
 
+board_view.draw();
 /**
  * Función principal del programa, desde donde se crean los elementos del juego 
  */
